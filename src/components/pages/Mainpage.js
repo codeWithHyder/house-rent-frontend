af97d48d466dfd7dd3898409bd39f42d4cfcc974
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-20 14:57:58 +0300

    Display house Items

--- src/components/pages/Mainpage.js
+++ src/components/pages/Mainpage.js
@@ -1,39 +1,38 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setHouses } from '../../Redux/actions';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const MainPage = () => {
   // const dispatch = useDispatch();
-  // const houses = useSelector((state) => state.houses);
+  const houses = useSelector((state) => state.house.houseData);
 
   // // In this example, we'll use useEffect to simulate fetching data from an API
-  // useEffect(() => {
-  //   // Simulate API call to get house data
-  //   // Replace this with actual API call in your project
-  //   import('../../houseData').then((module) => {
-  //     dispatch(setHouses(module.default));
-  //   });
-  // }, [dispatch]);
-
-  // return (
-  //   <>
-  //     <ul className="main-page">
-  //       {houses.map((house) => (
-  //         <li key={house.id}>
-  //           <img src={house.imageUrl} alt={house.title} />
-  //           {/* Replace the link with React Router Link */}
-  //           <a href={`/house/${house.id}`}>
-  //             <div className="house-item">
-  //               <div className="house-info">
-  //                 <h3>{house.title}</h3>
-  //                 <p>{house.description}</p>
-  //               </div>
-  //             </div>
-  //           </a>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </>
-  // );
+  // // useEffect(() => {
+  // //   // Simulate API call to get house data
+  // //   // Replace this with actual API call in your project
+  // //   import('../../houseData').then((module) => {
+  // //     dispatch(setHouses(module.default));
+  // //   });
+  // // }, [dispatch]);
+  console.log(houses);
+  return (
+    <>
+      <ul className="main-page">
+        {houses.map((house) => (
+          <li key={house.id}>
+            <img src={house.imageUrl} alt={house.title} />
+            {/* Replace the link with React Router Link */}
+            <a href={`/house/${house.id}`}>
+              <div className="house-item">
+                <div className="house-info">
+                  <h3>{house.title}</h3>
+                  <p>{house.description}</p>
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default MainPage;
