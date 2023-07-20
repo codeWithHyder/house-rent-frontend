3c27ed6eb1b6e27027c7d1532f576d8c368bbcf8
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-20 17:22:28 +0300

    Fix linter errors

--- src/Redux/feature/houseDetailsSlice.js
+++ src/Redux/feature/houseDetailsSlice.js
@@ -1,87 +1,90 @@
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const houseData = [
-    {
-      id: 1,
-      title: 'Beautiful House 1',
-      description: 'This is a beautiful house with a large garden.',
-      price: 250000,
-      imageUrl: 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
-    },
-    {
-      id: 2,
-      title: 'Cozy House 2',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://housing.com/news/wp-content/uploads/2022/11/shutterstock_1715891752-1200x700-compressed.jpg',
-    },
-    {
-      id: 3,
-      title: 'Cozy House 3',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://assets-news.housing.com/news/wp-content/uploads/2022/04/07013406/ELEVATED-HOUSE-DESIGN-FEATURE-compressed.jpg',
-    },
-    {
-      id: 4,
-      title: 'Cozy House 4',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://thumbs.dreamstime.com/z/beautiful-exterior-home-pictures-new-home-design-images-modern-best-house-design-images-best-house-images-images-latest-172194515.jpg',
-    },
-    {
-      id: 5,
-      title: 'Cozy House 5',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://images.coolhouseplans.com/plans/80523/80523-b440.jpg',
-    },
-    {
-      id: 6,
-      title: 'Cozy House 6',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://www.nobroker.in/blog/wp-content/uploads/2022/07/Small-House-Designs.jpg',
-    },
-    {
-      id: 7,
-      title: 'Cozy House 7',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZoRD0K8p5aZ0PH_03Qz0nxACra-IW2syxg&usqp=CAU',
-    },
-    {
-      id: 8,
-      title: 'Cozy House 8',
-      description: 'A cozy house with a fireplace and stunning view.',
-      price: 180000,
-      imageUrl: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bHV4dXJ5JTIwaG91c2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
-    },
-    // Add more house objects here...
+  {
+    id: 1,
+    title: 'Beautiful House 1',
+    description: 'This is a beautiful house with a large garden.',
+    price: 250000,
+    imageUrl: 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+  },
+  {
+    id: 2,
+    title: 'Cozy House 2',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://housing.com/news/wp-content/uploads/2022/11/shutterstock_1715891752-1200x700-compressed.jpg',
+  },
+  {
+    id: 3,
+    title: 'Cozy House 3',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://assets-news.housing.com/news/wp-content/uploads/2022/04/07013406/ELEVATED-HOUSE-DESIGN-FEATURE-compressed.jpg',
+  },
+  {
+    id: 4,
+    title: 'Cozy House 4',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://thumbs.dreamstime.com/z/beautiful-exterior-home-pictures-new-home-design-images-modern-best-house-design-images-best-house-images-images-latest-172194515.jpg',
+  },
+  {
+    id: 5,
+    title: 'Cozy House 5',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://images.coolhouseplans.com/plans/80523/80523-b440.jpg',
+  },
+  {
+    id: 6,
+    title: 'Cozy House 6',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://www.nobroker.in/blog/wp-content/uploads/2022/07/Small-House-Designs.jpg',
+  },
+  {
+    id: 7,
+    title: 'Cozy House 7',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZoRD0K8p5aZ0PH_03Qz0nxACra-IW2syxg&usqp=CAU',
+  },
+  {
+    id: 8,
+    title: 'Cozy House 8',
+    description: 'A cozy house with a fireplace and stunning view.',
+    price: 180000,
+    imageUrl: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bHV4dXJ5JTIwaG91c2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+  },
+  // Add more house objects here...
 ];
 
 const initialState = {
-    isLoading: false,
-    detailData: houseData,
-    error: '',
-  };
-  
-  const detailsSlice = createSlice({
-    name: 'details',
-    initialState,
-    extraReducers: (builder) => {
-      builder.addCase(getDetails.pending, (state) => {
-        state.isLoading = true;
-      });
-      builder.addCase(getDetails.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.detailData = action.payload;
-      });
-      builder.addCase(getDetails.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      });
-    },
+  isLoading: false,
+  detailData: houseData,
+  error: '',
+};
+
+const detailsSlice = createSlice({
+  name: 'details',
+  initialState,
+  reducer: {
+
+  },
+//   extraReducers: (builder) => {
+//     builder.addCase(getDetails.pending, (state) => {
+//       state.isLoading = true;
+//     });
+//     builder.addCase(getDetails.fulfilled, (state, action) => {
+//       state.isLoading = false;
+//       state.detailData = action.payload;
+//     });
+//     builder.addCase(getDetails.rejected, (state, action) => {
+//       state.isLoading = false;
+//       state.error = action.error.message;
+//     });
+//   },
 });
-  
+
 export default detailsSlice.reducer;
