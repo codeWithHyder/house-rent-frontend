5ef6b2c42ef8da7a2b4a856f48d5bd19978c5a68
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-26 12:25:49 +0300

    Fix linr=ter errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MainPage from './components/pages/Mainpage';
 import HouseDetails from './components/pages/HouseDetails';
 import ReservePage from './components/pages/ReservePage';
 import AddHouse from './components/pages/AddHouse';
-// import Navigation from './components/sidebar/Navigation';
+import Navigation from './components/sidebar/Navigation';
 import './style.css';
 import './navbar.css';
 import './mainPage.css';
@@ -15,9 +15,8 @@ function App() {
   return (
     <Router>
       <div className="main-container">
-        {/* <Navigation />
-  <h1 className="rent-house">Home Stay</h1> */}
-
+        <Navigation />
+        <h1 className="rent-house">Home Stay</h1>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/house/:id" element={<HouseDetails />} />
