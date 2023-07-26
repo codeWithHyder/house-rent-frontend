608db200e79cc1603a5f076e3ae4981124db8a55
Hyder <hyderali400@gmail.com>
2023-07-26 18:55:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/sidebar/Navigation';
 import MainPage from './components/pages/Mainpage';
 import HouseDetails from './components/pages/HouseDetails';
 import ReservePage from './components/pages/ReservePage';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Navigation from './components/sidebar/Navigation';
+import AddHouse from './components/pages/AddHouse';
 import './styling/style.css';
 import './styling/navbar.css';
 import './styling/mainPage.css';
@@ -13,20 +14,24 @@ import './styling/houseReserve.css';
 
 function App() {
   return (
-
-    <div className="main-container">
-      <Router>
+    <Router>
+      <div className="main-container">
         <Routes>
           <Route path="/sign_in" element={<Login />} />
           <Route path="/sign_up" element={<Register />} />
-          <Route element={<Navigation />}>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/house/:id" element={<HouseDetails />} />
-            <Route path="/reserve/:id" element={<ReservePage />} />
-          </Route>
         </Routes>
-      </Router>
-    </div>
+        <Navigation />
+        <h1 className="rent-house">Home Stay</h1>
+
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/house/:id" element={<HouseDetails />} />
+          <Route path="/reserve/:id" element={<ReservePage />} />
+          <Route path="/addhouse" element={<AddHouse />} />
+          {/* Add more routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/sidebar/Navigation.js
+++ src/components/sidebar/Navigation.js
@@ -1,37 +1,73 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../Redux/feature/UserSlice';
 
 const Navigation = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const token = localStorage.getItem('token');
+  // const token = localStorage.getItem('token');
+
+  const isLoggedIn = useSelector((state) => state.auth.loggedIn);
 
   const handleLogout = async () => {
-    if (token) {
+    if (isLoggedIn) {
       try {
         await dispatch(logoutUser());
-        navigate('/');
+        // No need to navigate here since the useEffect will handle the redirection
       } catch (error) {
         // Handle any potential errors during the logout process here
-        // console.error('Logout failed:', error);
+        // eslint-disable-next-line no-console
+        console.error('Logout failed:', error);
       }
     }
   };
 
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/sign_in'); // Redirect to '/sign_in' when not logged in
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <nav>
       <ul className="navbar">
-        <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmoDV73D3hV_4yUErBD746LWfh_cru5spf5g&usqp=CAU" alt="house" /></li>
-        <li><NavLink to="#" activeClassName="active-link" exact>House</NavLink></li>
-        <li><NavLink to="/reserve/:id" activeClassName="active-link">ReserveHouse</NavLink></li>
-        <li><NavLink to="/myreservations" activeClassName="active-link">MyReservations</NavLink></li>
-        <li><NavLink to="/addhouse" activeClassName="active-link">AddHouse</NavLink></li>
-        <li><NavLink to="/deletehouse" activeClassName="active-link">DeleteHouse</NavLink></li>
-        {token ? (
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmoDV73D3hV_4yUErBD746LWfh_cru5spf5g&usqp=CAU"
+            alt="house"
+          />
+        </li>
+        <li>
+          <NavLink to="/" activeClassName="active-link" exact>
+            House
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/reserve/:id" activeClassName="active-link">
+            ReserveHouse
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/myreservations" activeClassName="active-link">
+            MyReservations
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/addhouse" activeClassName="active-link">
+            AddHouse
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/deletehouse" activeClassName="active-link">
+            DeleteHouse
+          </NavLink>
+        </li>
+        {isLoggedIn ? (
           <li>
-            <button className="logout-btn" type="button" onClick={handleLogout}>Logout</button>
+            <button className="logout-btn" type="button" onClick={handleLogout}>
+              Logout
+            </button>
           </li>
         ) : (
           <li>
