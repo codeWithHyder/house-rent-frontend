f83c6d0c1b709b9066a1194dcc92af77adb91d7c
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-26 12:35:19 +0300

    Fix Linter errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MainPage from './components/pages/Mainpage';
 import HouseDetails from './components/pages/HouseDetails';
 import ReservePage from './components/pages/ReservePage';
 import AddHouse from './components/pages/AddHouse';
-import Navigation from './components/sidebar/Navigation';
 import './style.css';
 import './navbar.css';
 import './mainPage.css';
