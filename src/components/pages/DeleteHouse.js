833d0756778182739c2144af9cead778bd9dc2d6
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-26 12:10:47 +0300

    Add style to delete page

--- src/components/pages/DeleteHouse.js
+++ src/components/pages/DeleteHouse.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { deleteHouse, getHouses } from '../../Redux/feature/houseSlice';
+import '../styles/deletehouse.css';
 
 const DeleteHouse = () => {
   const houses = useSelector((store) => store.house.houseData);
@@ -14,17 +18,31 @@ const DeleteHouse = () => {
   };
 
   return (
-    <div>
-      <ul>
+    <div className="delete-house">
+      <div className="delete-nav">
+        <Link to="/">
+          <FontAwesomeIcon icon={faArrowLeft} className="fa-arrow-left" />
+        </Link>
+        <h1>Delete a house</h1>
+      </div>
+
+      <ul className="delete-house-container">
         {houses.map((house) => (
-          <li key={house.id}>
+          <li key={house.id} className="delete-house-items">
             <img src={house.image_url} alt={house.name} />
-            Name:
-            {' '}
-            {house.name}
-            price:
-            {' '}
-            {house.rental_fee}
+            <div className="delete-items-details">
+              Name:
+              {' '}
+              {house.name}
+              <br />
+              Price:
+              {' '}
+              {house.rental_fee}
+              <br />
+              Location:
+              {' '}
+              {house.location}
+            </div>
             <button type="button" onClick={() => handleDeleteHouse(house.id)}>Delete</button>
           </li>
         ))}
