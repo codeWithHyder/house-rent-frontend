78027d34434dfdc24dd1bb18a94530aa4307e718
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-26 12:31:17 +0300

    Fix Linter errors

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import MainPage from './components/pages/Mainpage';
 import HouseDetails from './components/pages/HouseDetails';
 import ReservePage from './components/pages/ReservePage';
 import AddHouse from './components/pages/AddHouse';
-<<<<<<< HEAD
 import Navigation from './components/sidebar/Navigation';
 import './style.css';
 import './navbar.css';
 import './mainPage.css';
 import './houseReserve.css';
-=======
->>>>>>> c6cc5e4c9bc5d5520ee3a03acbcb1228712b5cb9
 import DeleteHouse from './components/pages/DeleteHouse';
-
-=======
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Navigation from './components/sidebar/Navigation';
@@ -25,26 +20,13 @@ import './styling/houseReserve.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <div className="main-container">
-        <Navigation />
-        <h1 className="rent-house">Home Stay</h1>
-=======
-
-
     <div className="main-container">
       <Router>
->>>>>>> c6cc5e4c9bc5d5520ee3a03acbcb1228712b5cb9
         <Routes>
-
           <Route path="/" element={<MainPage />} />
-          <Route path="/house/:id" element={<HouseDetails />} />
-          <Route path="/reserve/:id" element={<ReservePage />} />
           <Route path="/addhouse" element={<AddHouse />} />
           <Route path="/deletehouse" element={<DeleteHouse />} />
           {/* Add more routes here */}
-=======
           <Route path="/sign_in" element={<Login />} />
           <Route path="/sign_up" element={<Register />} />
           <Route element={<Navigation />}>
@@ -52,7 +34,6 @@ function App() {
             <Route path="/house/:id" element={<HouseDetails />} />
             <Route path="/reserve/:id" element={<ReservePage />} />
           </Route>
-
         </Routes>
       </Router>
     </div>
