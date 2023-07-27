f6ecaa47bda60fa9b3a437e62f9ebc9cb2d7162e
Thiri <thirihtet246@gmail.com>
2023-07-27 18:50:07 +0630

    Set up for user authorization

---
+++ src/setupAxiosInterceptors.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+
+const setupAxiosInterceptors = () => {
+  axios.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token'); // Replace with your code to get the JWT token
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      } else {
+        // If no token is available, remove the "Authorization" header
+        delete config.headers.Authorization;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error),
+  );
+};
+
+export default setupAxiosInterceptors;
