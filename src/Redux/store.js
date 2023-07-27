8ab1a33222ee0924578c5ab3de252810b39bb49f
Thiri <thirihtet246@gmail.com>
2023-07-27 18:49:29 +0630

    Remove member slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,7 +4,6 @@ import authReducer from './feature/UserSlice';
 import signUpReducer from './feature/regestrationSlice';
 // import addHouseSlice from './feature/addHouseSlice';
 import reservationsReducer from './feature/reservationsSlice';
-import memeberReducer from './feature/memeberSlice'
 
 const store = configureStore({
   reducer: {
@@ -13,9 +12,7 @@ const store = configureStore({
     register: signUpReducer,
     // addHouse: addHouseSlice,
     reservations: reservationsReducer,
-    member: memeberReducer,
   },
-
 });
 
 export default store;
