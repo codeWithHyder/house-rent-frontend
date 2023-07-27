b6c12726b2e97d584a8dd48632df9f26084c830b
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-27 16:22:56 +0300

    Fix merge conflict and inter errors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/pages/Mainpage';
 import HouseDetails from './components/pages/HouseDetails';
 import ReservePage from './components/pages/ReservePage';
-import AddHouse from './components/pages/AddHouse';
-import './style.css';
-import './navbar.css';
-import './mainPage.css';
-import './houseReserve.css';
+// import './style.css';
+// import './navbar.css';
+// import './mainPage.css';
+// import './houseReserve.css';
 import DeleteHouse from './components/pages/DeleteHouse';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -22,14 +21,6 @@ function App() {
   return (
     <Router>
       <div className="main-container">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/addhouse" element={<AddHouse />} />
-          <Route path="/deletehouse" element={<DeleteHouse />} />
-          {/* Add more routes here */}
-          <Route path="/sign_in" element={<Login />} />
-          <Route path="/sign_up" element={<Register />} />
-        </Routes>
         <Navigation />
         <h1 className="rent-house">Home Stay</h1>
 
@@ -38,8 +29,9 @@ function App() {
           <Route path="/house/:id" element={<HouseDetails />} />
           <Route path="/reserve/:id" element={<ReservePage />} />
           <Route path="/addhouse" element={<AddHouse />} />
-
-          {/* Add more routes here */}
+          <Route path="/deletehouse" element={<DeleteHouse />} />
+          <Route path="/sign_in" element={<Login />} />
+          <Route path="/sign_up" element={<Register />} />
         </Routes>
       </div>
     </Router>
