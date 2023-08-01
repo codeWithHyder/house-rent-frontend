93e230d0c19197a1cb551a0cc70e8193e1fe8f96
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-08-01 13:03:43 +0300

    Fix linter errors

--- src/test/mainPage.test.js
+++ src/test/mainPage.test.js
@@ -9,112 +9,109 @@ import MainPage from '../components/pages/Mainpage';
 import houseSlice from '../Redux/feature/houseSlice';
 
 const initialState = {
-    house: {
-      houseData: [
-        {
-          id: 1,
-          category: 'Category 1',
-          description: 'Description 1',
-          image_url: 'url1',
-        },
-        {
-          id: 2,
-          category: 'Category 2',
-          description: 'Description 2',
-          image_url: 'url2',
-        },
-        // Add more test data as needed
-      ],
-      isLoading: false,
-    },
+  house: {
+    houseData: [
+      {
+        id: 1,
+        category: 'Category 1',
+        description: 'Description 1',
+        image_url: 'url1',
+      },
+      {
+        id: 2,
+        category: 'Category 2',
+        description: 'Description 2',
+        image_url: 'url2',
+      },
+      // Add more test data as needed
+    ],
+    isLoading: false,
+  },
 };
-  
+
 // Create the Redux store with Redux Toolkit's configureStore
 const store = configureStore({
-    reducer: {
-        house: houseSlice, // Add other reducers here if needed
+  reducer: {
+    house: houseSlice, // Add other reducers here if needed
+  },
+  preloadedState: initialState,
+  middleware: [thunk], // Add other middlewares here if needed
+});
+
+// Test if the loading spinner is displayed when isLoading is true
+test('renders loading spinner when data is loading', () => {
+  const loadingState = {
+    house: {
+      ...initialState.house,
+      isLoading: true,
     },
-    preloadedState: initialState,
-    middleware: [thunk], // Add other middlewares here if needed
+  };
+  const loadingStore = configureStore({
+    reducer: {
+      house: houseSlice,
+    },
+    preloadedState: loadingState,
+    middleware: [thunk],
+  });
+
+  render(
+    <Provider store={loadingStore}>
+      <MainPage />
+    </Provider>,
+  );
+
+  const loadingElement = screen.getByText('Loading...');
+  expect(loadingElement).toBeInTheDocument();
 });
-  
-  // Test if the loading spinner is displayed when isLoading is true
-  test('renders loading spinner when data is loading', () => {
-    const loadingState = {
-      house: {
-        ...initialState.house,
-        isLoading: true,
-      },
-    };
-    const loadingStore = configureStore({
-      reducer: {
-        house: houseSlice,
-      },
-      preloadedState: loadingState,
-      middleware: [thunk],
-    });
-  
-    render(
-      <Provider store={loadingStore}>
+
+// Test if the house items are rendered correctly when data is available
+test('renders next arrow', async () => {
+  const history = createMemoryHistory();
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter history={history}>
         <MainPage />
-      </Provider>
-    );
-  
-    const loadingElement = screen.getByText('Loading...');
-    expect(loadingElement).toBeInTheDocument();
+      </MemoryRouter>
+    </Provider>,
+  );
+
+  await waitFor(() => {
+    const arrowElement = screen.queryByTestId('nxt-arrow');
+    expect(arrowElement).toBeInTheDocument();
+  });
 });
-
-
-  // Test if the house items are rendered correctly when data is available
-  test('renders next arrow', async () => {
-    const history = createMemoryHistory();
-  
-    render(
-        <Provider store={store}>
-            <MemoryRouter history={history}>
-                <MainPage />
-            </MemoryRouter>
-      </Provider>
-    );
-  
-    await waitFor(() => {
-        const arrowElement = screen.queryByTestId('nxt-arrow');
-        expect(arrowElement).toBeInTheDocument();
-    });
-});
-
 
 test('renders prev arrow', async () => {
-    const history = createMemoryHistory();
-  
-    render(
-        <Provider store={store}>
-            <MemoryRouter history={history}>
-                <MainPage />
-            </MemoryRouter>
-      </Provider>
-    );
-  
-    await waitFor(() => {
-        const arrowElement = screen.queryByTestId('prev-arrow');
-        expect(arrowElement).toBeInTheDocument();
-    });
+  const history = createMemoryHistory();
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter history={history}>
+        <MainPage />
+      </MemoryRouter>
+    </Provider>,
+  );
+
+  await waitFor(() => {
+    const arrowElement = screen.queryByTestId('prev-arrow');
+    expect(arrowElement).toBeInTheDocument();
+  });
 });
 
-
 test('renders List items', async () => {
-    const history = createMemoryHistory();
-  
-    render(
-        <Provider store={store}>
-            <MemoryRouter history={history}>
-                <MainPage />
-            </MemoryRouter>
-      </Provider>
-    );
-  
-    await waitFor(() => {
-        const listItems = screen.getAllByTestId('listitem');
-        expect(listItems).toHaveLength(2);
-    });
+  const history = createMemoryHistory();
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter history={history}>
+        <MainPage />
+      </MemoryRouter>
+    </Provider>,
+  );
+
+  await waitFor(() => {
+    const listItems = screen.getAllByTestId('listitem');
+    expect(listItems).toHaveLength(2);
+  });
 });
