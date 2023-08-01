ccdd86cd5ff7ce1780489a27317a3afb9a976db4
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-08-01 10:55:56 +0300

    Fix linter errors

--- src/test/deleteHouse.test.js
+++ src/test/deleteHouse.test.js
@@ -2,26 +2,31 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { MemoryRouter } from 'react-router-dom'; 
+import { MemoryRouter } from 'react-router-dom';
 import DeleteHouse from '../components/pages/DeleteHouse';
 import houseReducer from '../Redux/feature/houseSlice';
 
 const renderWithRedux = (
   ui,
-  { initialState, store = configureStore({ reducer: houseReducer, preloadedState: initialState }) } = {},
+  {
+    initialState, store = configureStore(
+      {
+        reducer: houseReducer, preloadedState: initialState,
+      },
+    ),
+  } = {},
 ) => ({
   ...render(
     <Provider store={store}>
       <MemoryRouter>
         {ui}
       </MemoryRouter>
-    </Provider>
+    </Provider>,
   ),
   store,
 });
 
 describe('DeleteHouse component', () => {
-
   let mockHousesData;
   let container;
 
@@ -51,28 +56,28 @@ describe('DeleteHouse component', () => {
       },
     }));
   });
-  
+
   // Ensure the component is rendered properly
   it('should render correctly with houses data', () => {
     expect(container).toBeInTheDocument();
   });
 
   // Ensure the delete-nav elements are rendered
-  it('delete-nav elements are rendered', () =>{
+  it('delete-nav elements are rendered', () => {
     expect(screen.getByText('Delete a house')).toBeInTheDocument();
     expect(screen.getByTestId('fa-arrow-left')).toBeInTheDocument();
   });
 
   // Ensure the delete-house-container elements are rendered
-  it('delete-house-container elements are rendered', () =>{
+  it('delete-house-container elements are rendered', () => {
     expect(screen.getByTestId('delete-house-container')).toBeInTheDocument();
     expect(screen.getAllByTestId('delete-house-item')).toHaveLength(2);
   });
-      
+
   // Ensure the delete button is rendered
-  it('delete button is rendered', () =>{
+  it('delete button is rendered', () => {
     expect(screen.getAllByTestId('delete-button')).toHaveLength(2);
     expect(screen.getAllByTestId('delete-button')[0]).toHaveTextContent('Delete');
     expect(screen.getAllByTestId('delete-button')[1]).toHaveTextContent('Delete');
-  });    
+  });
 });
