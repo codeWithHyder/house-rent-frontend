a11d728bc67e04e90b04154e52f7f9501ec96242
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-08-02 12:07:53 +0300

    Fix add house test failure

--- src/test/addHouse.test.js
+++ src/test/addHouse.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
