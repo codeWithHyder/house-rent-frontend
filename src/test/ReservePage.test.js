b43672839ddb6377117366a78ceb6a0fe3ca6b58
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-08-02 12:17:32 +0300

    Fix mainpage test failure

--- src/test/ReservePage.test.js
+++ src/test/ReservePage.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
